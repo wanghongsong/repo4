e8485324d1427c2dbc84fbb7224a54362769d418
admin <1336422934@qq.com>
2018-12-04 20:57:28 +0800

    添加demo

---
+++ src/com/whs/Demo.java
@@ -0,0 +1,9 @@
+package com.whs;
+
+public class Demo {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
