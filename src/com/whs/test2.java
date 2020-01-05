a6a905028b5976afdcdbf74e84fd2a596b6d3730
wanghongsong <45525884+wanghongsong@users.noreply.github.com>
2020-01-05 20:15:23 +0800

    Delete test2.java

--- src/com/whs/test2.java
+++
@@ -1,9 +0,0 @@
-package com.whs;
-
-public class test2 {
-    public static void main(String[] args) {
-        System.out.println(222222);
-        System.out.println(333333);
-        System.out.println(777777);
-    }
-}
